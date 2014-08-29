f7ecf41a37374cd867c7557e98a738e270dde3f9
Jun Kimura <jksmphone@gmail.com>
2014-08-29 22:23:06 +0900

    Added Mul method.

--- tests/app.go
+++ tests/app.go
@@ -3,3 +3,7 @@ package tests
 func Add(x, y int) int {
 	return x + y
 }
+
+func Mul(x, y int) int {
+	return 1
+}
--- tests/app_test.go
+++ tests/app_test.go
@@ -9,3 +9,9 @@ func TestAdd(t *testing.T) {
 		t.Error("expected value: 30")
 	}
 }
+
+func TestMul(t *testing.T) {
+	if Mul(10, 20) != 200 {
+		t.Error("expected value: 200")
+	}
+}
